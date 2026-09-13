3c49c71801063ccf9531e1ce2e84f5abb9f816eb
taaruntd <tarun.dhingra519@gmail.com>
2026-09-13 22:23:21 +0530

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,6 +1,11 @@
-export default function Logo({compact=false}){
-  return <div className={`logo ${compact?'compact':''}`}>
-    <div className="mark"><span>Z</span></div>
-    <div className="logoWords"><b>ZEAL</b><small>CONSULTING</small></div>
-  </div>
+export default function Logo({ compact = false }) {
+  return (
+    <div className={`logo ${compact ? 'compact' : ''}`}>
+      <img
+        src="/zeal-logo.png"
+        alt="Zeal Consulting"
+        className="realLogo"
+      />
+    </div>
+  )
 }
